35d126257843d33193c91d04fe4cf66c58bae398
Abhishek <abhishek.patel131@gmail.com>
2011-04-21 18:08:09 +0530

    try with ticket.rb

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,13 @@
 class Ticket < ActiveRecord::Base
   #belongs_to :user
   belongs_to :assigned, :foreign_key => "assigned_to", :class_name => "User"
-  belongs_to :owner, :foreign_key => "owners_id", :class_name => "User"
+  belongs_to :owner, :foreign_key => "owner_id", :class_name => "User"
   has_many :comments
   belongs_to :department
   #has_associated_audits
+
+
+  def assign_to_user(user_id)
+    update_attribute(:assigned_to,user_id)
+  end
 end
